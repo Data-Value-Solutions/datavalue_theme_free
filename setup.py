4cd53ce7dafbdb444245fcc3bce28c8414573ff7
Abdo Hamoud <abdo.host@gmail.com>
2026-04-21 00:05:48 +0200

    Add setup.py for datavalue_theme_free package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datavalue_theme_free",
+    version="1.0.0",
+    description="Free theme for Frappe",
+    author="Abdo Hamoud",
+    author_email="abdo.host@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "frappe>=15.0.0,<16.0.0"
+    ]
+)
